20405df66e6f32dbf74b20df43fa9728edb07617
Samuel <samuel@rectmedia.com>
2022-04-12 23:17:43 +0700

    Chore:[SK] laravel backpack

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    private $database;
+
+    public function __construct()
+    {
+        $this->database = \App\Services\FirebaseService::connect();
+    }
+
+    public function create(Request $request) 
+    {
+        $this->database
+            ->getReference('test/blogs/' . $request['title'])
+            ->set([
+                'title' => $request['title'] ,
+                'content' => $request['content']
+            ]);
+
+        return response()->json('blog has been created');
+    }
+    public function index() 
+    {
+        return response()->json($this->database->getReference('test/blogs')->getValue());
+    }
+}
