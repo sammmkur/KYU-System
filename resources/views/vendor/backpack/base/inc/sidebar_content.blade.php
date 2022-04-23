b92bfc9ec3bf9f7dab2c23d47a258c2fc200bc64
Samuel <samuel@rectmedia.com>
2022-04-23 14:57:04 +0700

    feat:[SK] absensi controller

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -9,3 +9,4 @@
         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon la la-key"></i> <span>Permissions</span></a></li>
     </ul>
 </li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('absensi') }}'><i class='nav-icon la la-question'></i> Absensi</a></li>
